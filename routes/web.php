29454a8e6fdaf76a549716bbb04eb0b2b6966c39
40115 <57526647+40115@users.noreply.github.com>
2021-12-01 19:36:31 +0000

    Laravel

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/Login', function () {
+    return view('Login');
+});
+Route::get('/Registo', function () {
+    return view('Registo');
+});
+Route::get('/Main', function () {
+    return view('Main');
+});
