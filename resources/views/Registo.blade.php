dbcd2b495920b779d3f691fd1491ed11446c948c
40115 <57526647+40115@users.noreply.github.com>
2021-12-06 18:23:19 +0000

    Ruben (#2)
    * Laravel
    
    * laravel
    
    * m

---
+++ resources/views/Registo.blade.php
@@ -0,0 +1,76 @@
+@extends('Layout.Model')
+
+@section('Title')
+Registo
+@endsection
+
+@section('Content')
+<div class ="Regis_log_form">
+    <h1 class= "Title">
+    Register Utilizador
+    </h1>
+
+    <div class="row">
+        <form class="col s12" action ="/Utilizador" method="post" >
+            {{ csrf_field() }}
+          <div class="row">
+            <div class="input-field col s12">
+                <em class="material-icons prefix">email</em>
+              <input id="email" type="text" name="email" class="validate">
+              <label for="email">Email</label>
+              <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+            </div>
+          </div>
+          <div class="row">
+          <div class="input-field col s12">
+            <em class="material-icons prefix">people</em>
+            <select name="type">
+              <option value="Admin" id="OP-A">Admin</option>
+              <option value="Docente" id="OP-D">Docente</option>
+              <option selected="selected" value="Aluno" id="OP-A">Aluno</option>
+            </select>
+            <label>Type</label>
+          </div>
+        </div>
+
+
+          <div class="row">
+            <div class="input-field col s12">
+                <em class="material-icons prefix">password</em>
+              <input id="password" type="text" name="password" minlength="10"  class="validate" Password="*">
+              <label for="password">Password</label>
+            </div>
+
+          </div>
+
+          <div class="row">
+            <div class="input-field col s12">
+                <em class="material-icons prefix">password</em>
+              <input id="Re-password" type="text"name="re-password" minlength="10"  class="validate">
+              <label for="password">Re-Password</label>
+            </div>
+
+          </div>
+
+
+
+                <input  type="submit"  value="Register" />
+
+
+
+
+        </form>
+        <div class="Subtitle">
+            <a href="{{url('/logI' )}}" class="Subtitle" >Log In</a>
+            </div>
+
+
+
+
+      </div>
+
+      </div>
+
+
+
+@endsection
